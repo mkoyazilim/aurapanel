7bc03160ebdeead9beb287c000e86d4d241e3a78
mkoyazilim <267345287+mkoyazilim@users.noreply.github.com>
2026-08-14 14:51:19 +0300

    build: compile SFTP ui components

--- internal/webui/dist/assets/razor-B4rLloNL.js
+++ internal/webui/dist/assets/razor-B4rLloNL.js
@@ -1,4 +1,4 @@
-import{m}from"./editor.main-BQSXVlyh.js";import"./index-CkLxnSqF.js";/*!-----------------------------------------------------------------------------
+import{m}from"./editor.main-qw4UH8oV.js";import"./index-Bdjm7wM7.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
