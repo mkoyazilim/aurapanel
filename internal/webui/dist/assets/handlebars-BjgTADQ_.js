4448b59a7c6e6850a876c146f87a7a2004c655a6
mkoyazilim <267345287+mkoyazilim@users.noreply.github.com>
2026-08-14 14:34:20 +0300

    fix(ui): fix button styles and build frontend

--- internal/webui/dist/assets/handlebars-BjgTADQ_.js
+++ internal/webui/dist/assets/handlebars-BjgTADQ_.js
@@ -1,4 +1,4 @@
-import{m as i}from"./editor.main-Cw-xXiBM.js";import"./index-C17BosPW.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./editor.main-f1dNKyli.js";import"./index-DwREX4J8.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
