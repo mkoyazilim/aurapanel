dac93637bdde36002d1b6de558e4a3bc3bc8f4ef
mkoyazilim <267345287+mkoyazilim@users.noreply.github.com>
2026-08-17 00:25:56 +0300

    fix(ui): inject useI18n into Mail.vue to fix blank screen

--- web/src/views/Mail.vue
+++ web/src/views/Mail.vue
@@ -225,6 +225,8 @@
 
 <script setup>
 import { ref, computed, onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
 import Layout from '../components/Layout.vue'
 import { api } from '../api'
 
